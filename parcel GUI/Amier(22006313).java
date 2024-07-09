c5bf23429db4c7669418f86a69c7ae7d6a676ef4
ironk-0 <farhanizamfairus@gmail.com>
2024-07-09 21:38:15 +0800

    Update Amier(22006313).java

--- parcel GUI/Amier(22006313).java
+++ parcel GUI/Amier(22006313).java
@@ -270,13 +270,6 @@ public class UserPayment extends javax.swing.JFrame {
         selectedBank = "HongLeongBank";
     }                                  
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new UserPayment().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
